1a4b702670f2d5d3183e7d332c3e37f0d08bc74f
kevahere <kevahere@gmail.com>
2018-09-28 15:43:25 +0300

    added tests for init user

--- user_tests.py
+++ user_tests.py
@@ -1,2 +1,17 @@
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+    '''Test class for user behaviour'''
+
+    def setUp(self):
+        """Checking the user is instantiated correctly"""
+        self.new_user = User("Kevin","Ahere")
+
+    def test_init(self):
+        """Checking for the proper init"""
+        self.assertEqual(self.new_user.first_name,"Kevin")
+        self.assertEqual(self.new_user.second_name,"Ahere")
+
+
+
