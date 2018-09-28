e81d43405f5e6424f620152ef4b37ef151c15154
kevahere <kevahere@gmail.com>
2018-09-28 15:49:34 +0300

    added tests for deleting users,finding users and teardown method

--- user_tests.py
+++ user_tests.py
@@ -14,4 +14,38 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.second_name,"Ahere")
 
 
+    def test_save_user(self):
+        """Checking to see if we can add users"""
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
 
+    def tearDown(self):
+        '''
+        emptys the list after running tests
+        :return:
+        '''
+        User.user_list = []
+
+    def test_delete_user(self):
+        '''
+        checking if users are deleted correctly
+        :return:
+        '''
+        self.new_user.save_user()
+        test_user = User("Kevinov", "Aherestan")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list), 1)
+
+    def test_find_user(self):
+        self.new_user.save_user()
+        test_user = User("Kevinov", "Aherestan")
+        test_user.save_user()
+
+        found_user = User.find_user("Kevinov")
+
+        self.assertEqual(found_user.last_name, test_user.last_name)
+
+if __name__ == '__main__':
+    unittest.main()
