0c79fcafb7843e5347e6d8e01709afdb0b4b4e0e
kevahere <kevahere@gmail.com>
2018-10-01 09:39:28 +0300

    tested the init

--- credentials_tests.py
+++ credentials_tests.py
@@ -7,3 +7,8 @@ class TestCredentials(unittest.TestCase):
     def setUp(self):
         '''runs before each test case'''
         self.new_credentials = Credentials("insta", "kevahere", "3nd0fDay5")
+
+    def test_init(self):
+        self.assertEqual(self.new_credentials.username, "kevahere")
+        self.assertEqual(self.new_credentials.password, "3nd0fDay5")
+        self.assertTrue(self.new_credentials.application, "Insta")
