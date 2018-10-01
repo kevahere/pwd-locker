eba518ac1a96bc60e9d53631264b0c7e24bdb7b3
kevahere <kevahere@gmail.com>
2018-10-01 09:37:39 +0300

    added class and setup

--- credentials_tests.py
+++ credentials_tests.py
@@ -1,3 +1,9 @@
 import unittest
 import pyperclip
 from credentials import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        '''runs before each test case'''
+        self.new_credentials = Credentials("insta", "kevahere", "3nd0fDay5")
